dddb0f71296cd1d8df6eee42b9be9be6269e2d56
Fei Yuan <yuan@nscl.msu.edu>
2016-09-13 16:59:31 -0400

    REGEX

---
+++ fix.py
@@ -0,0 +1,42 @@
+import subprocess, sys
+sys.path.insert(1, "../_urandom")
+from utils import *
+
+fn = "src/imsrg.hpp"
+s = load_file(fn)
+
+s = subprocess.run(["clang-format"], input=s, check=True,
+                   stdout=subprocess.PIPE, universal_newlines=True).stdout
+
+# remove the 'rfl' stuff
+s = re.sub(r"brfl\((B\d\[l\w+\])\((u\w+), (u\w+)\)\)",
+           r"\1(\3, \2)", s)
+s = re.sub(r"arfl\((A\d\[l\w+\])\((u\w+), (u\w+)\)\)",
+           r"\1(\3, \2)", s)
+
+# remove the signed view indices
+s = re.sub(r"\b(u\w+)_\b",
+           r"\1", s)
+
+# convert getters to new format
+s = re.sub(r"([A-Z])\d(\d?\[)", r"\1\2", s)
+s = re.sub(r"(\w+)\[l\w\]\(u(\w), u(\w)\)",
+           r"GET_1(\1, \2, \3)", s)
+s = re.sub(r"(\w+)\[l\w\w\]\(u(\w)(\w), u(\w)(\w)\)",
+           r"GET_2(\1, \2, \3, \4, \5)", s)
+
+s = re.sub(r"for \(auto &&l(\w) : basis_1\.channels\(\)\)",
+           r"FOR_CHANNEL_1(\1) /* TODO: remove me */", s)
+s = re.sub(r"for \(auto &&l(\w)(\w) : basis_2\.channels\(\)\)",
+           r"FOR_CHANNEL_2(\1, \2) /* TODO: remove me */", s)
+
+s = re.sub(r"for \(auto &&u(\w) : basis_1.subindices\(l(\w)\)\)",
+           r"FOR_STATE_1(\1) /* l\1 <- l\2 */", s)
+s = re.sub(r"for \(auto &&u(\w)(\w) : basis_1.subindices\(l(\w)(\w)\)\)",
+           r"FOR_STATE_2(\1, \2) /* l\1\2 <- l\3\4 */", s)
+
+s = subprocess.run(["clang-format"], input=s, check=True,
+                   stdout=subprocess.PIPE, universal_newlines=True).stdout
+
+print(s)
+save_file(fn, s)
