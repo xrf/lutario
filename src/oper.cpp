d08a3a8c7489402a1e26095494ad0da3d9e51ce6
Fei Yuan <yuan@nscl.msu.edu>
2016-10-04 17:37:45 -0400

    Turn (ManyBody)?Oper structs into classes

--- src/oper.cpp
+++ src/oper.cpp
@@ -4,24 +4,26 @@
 
 AllocReqBatch<double> Oper::alloc_req(const ManyBodyBasis &mbasis, OperKind kk)
 {
+    this->_many_body_basis = &mbasis;
     Rank r = oper_kind_to_rank(kk);
     size_t nl = mbasis.table().num_channels(r);
-    this->blocks.resize(nl);
+    this->_blocks.resize(nl);
     AllocReqBatch<double> reqs;
     for (size_t l = 0; l < nl; ++l) {
         size_t nu1, nu2;
         mbasis.block_size(kk, l, &nu1, &nu2);
-        reqs.push(this->blocks[l].alloc_req(nu1, nu2));
+        reqs.push((*this)[l].alloc_req(nu1, nu2));
     }
     return reqs;
 }
 
 AllocReqBatch<double> ManyBodyOper::alloc_req(const ManyBodyBasis &mbasis)
 {
+    this->_many_body_basis = &mbasis;
     AllocReqBatch<double> reqs;
     for (OperKind kk : {OPER_KIND_000, OPER_KIND_100, OPER_KIND_200}) {
         Rank r = oper_kind_to_rank(kk);
-        reqs.push(this->opers[r].alloc_req(mbasis, kk));
+        reqs.push((*this)[r].alloc_req(mbasis, kk));
     }
     return reqs;
 }
--- src/oper.hpp
+++ src/oper.hpp
@@ -4,11 +4,32 @@
 #include "matrix.hpp"
 #include "basis.hpp"
 
-struct Oper {
+class Oper {
 
-    std::vector<Matrix<double>> blocks;
+    const ManyBodyBasis *_many_body_basis = nullptr;
+
+    std::vector<Matrix<double>> _blocks;
+
+public:
 
     AllocReqBatch<double> alloc_req(const ManyBodyBasis &mbasis, OperKind kk);
+
+    const ManyBodyBasis &many_body_basis() const
+    {
+        return *this->_many_body_basis;
+    }
+
+    const Matrix<double> &operator[](size_t l) const
+    {
+        assert(l < this->_blocks.size());
+        return this->_blocks[l];
+    }
+
+    Matrix<double> &operator[](size_t l)
+    {
+        assert(l < this->_blocks.size());
+        return this->_blocks[l];
+    }
 
 };
 
@@ -21,11 +42,32 @@ struct Oper {
 ///
 ///   - Two-body operator in 200 form.
 ///
-struct ManyBodyOper {
+class ManyBodyOper {
 
-    Oper opers[3];
+    const ManyBodyBasis *_many_body_basis = nullptr;
+
+    Oper _opers[3];
+
+public:
 
     AllocReqBatch<double> alloc_req(const ManyBodyBasis &mbasis);
+
+    const ManyBodyBasis &many_body_basis() const
+    {
+        return *this->_many_body_basis;
+    }
+
+    const Oper &operator[](size_t r) const
+    {
+        assert(r < 3);
+        return this->_opers[r];
+    }
+
+    Oper &operator[](size_t r)
+    {
+        assert(r < 3);
+        return this->_opers[r];
+    }
 
 };
 
