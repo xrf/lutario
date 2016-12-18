240c14bcf49655274c54d1362d2c4e6632d9b802
Fei Yuan <yuan@nscl.msu.edu>
2016-12-17 22:07:11 -0500

    Add pmorph to src/utility

--- src/utility.cpp
+++ src/utility.cpp
@@ -1,5 +1,49 @@
+#include <math.h>
+#include <stdint.h>
 #include <stdio.h>
 #include "utility.hpp"
+
+uint64_t xorshift128plus(uint64_t *state)
+{
+    uint64_t x = state[0];
+    uint64_t y = state[1];
+    uint64_t r = x + y;
+    uint64_t p = x ^ x << 23;
+    uint64_t q = p ^ y ^ p >> 18 ^ y >> 5;
+    state[0] = y;
+    state[1] = q;
+    return r;
+}
+
+void pmorph(const double *array, size_t len, double *hash_out, size_t hash_len)
+{
+    uint64_t state[] = {0x0d4b6b5eeea339da, 0x90186095efbf5532};
+    double coeff = 1.0 / sqrt(hash_len);
+    size_t i, j, k;
+    for (i = 0; i < hash_len; ++i) {
+        hash_out[i] = 0.0;
+    }
+    for (j = 0; j < len; ++j) {
+        double array_j = array[j];
+        for (i = 0; i < hash_len; i += 32) {
+            uint64_t u = xorshift128plus(state);
+            size_t k_stop = k < hash_len - i ? k : hash_len - i;
+            for (k = 0; k < k_stop; ++k) {
+                /* use only the upper 32 bits of xorshift128 because the
+                   randomness of the lowest bit is slightly deficient */
+                uint64_t positive = u & ((uint64_t)1 << (63 - k));
+                if (positive) {
+                    hash_out[i + k] += array_j;
+                } else {
+                    hash_out[i + k] -= array_j;
+                }
+            }
+        }
+    }
+    for (i = 0; i < hash_len; ++i) {
+        hash_out[i] *= coeff;
+    }
+}
 
 void FileDeleter::operator()(FILE *stream) const
 {
--- src/utility.hpp
+++ src/utility.hpp
@@ -1,9 +1,31 @@
 #ifndef UTILITY_HPP
 #define UTILITY_HPP
+#include <stdint.h>
 #include <memory>
 #include <ostream>
 #include <tuple>
 #include <vector>
+
+/// Pseudorandom number generator using the xorshift128+ algorithm.  It is a
+/// very fast high-quality non-cryptographic random number generator.
+/// https://doi.org/10.1016/j.cam.2016.11.006
+///
+/// The state must be seeded so that it is not everywhere zero.  Better yet,
+/// pick a seed from a hardware source (e.g. /dev/urandom).  Seeds that are
+/// close to zero will generate poor quality random numbers for a few hundred
+/// iterations or so.
+///
+/// Note that the lowest bit is slightly less random than the other bits.
+uint64_t xorshift128plus(uint64_t *state);
+
+/// Plus-minus-one random projection hash is a locality sensitive hash
+/// function for floating-point numbers.  It uses a matrix of +1 and -1
+/// generated via xorshift128+ with a fixed seed.
+///
+/// As with random projection methods in general, the resulting hash is linear
+/// with respect to the original vector.  Moreover, norms in the hash space
+/// give a rough (+/-50%) approximation of norms in the original space.
+void pmorph(const double *array, size_t len, double *hash_out, size_t hash_len);
 
 /// A function object that closes a `FILE *`.
 struct FileDeleter
